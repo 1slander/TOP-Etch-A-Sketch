4041504edc41666796e22fab188676b3b93d68d3
1slander <hectorgomendez@gmail.com>
2022-11-23 09:33:34 +0000

    Added event listener

--- script.js
+++ script.js
@@ -4,9 +4,22 @@
 
 const container = document.querySelector(".container");
 
-// Add 16 divs
+// Add Grid = 16x16
+
 for (let i = 1; i < 273; i++) {
   const div = document.createElement("div");
   div.classList.add("container-grid");
   container.append(div);
+}
+
+// Select Divs inside container
+
+const colorGrid = document.getElementsByClassName("container-grid");
+
+// Event Listener for changing background color
+
+for (let j = 0; j < colorGrid.length; j++) {
+  colorGrid[j].addEventListener("mouseover", function () {
+    colorGrid[j].style.backgroundColor = "black";
+  });
 }
