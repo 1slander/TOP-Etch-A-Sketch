ddf08443da55e5ec9d392cc55d7a395f89086a0e
1slander <hectorgomendez@gmail.com>
2022-12-03 12:50:56 +0000

    Edit code

--- script.js
+++ script.js
@@ -11,6 +11,41 @@ showGridSize.textContent = `Actual size: ${gridSizeNum} x ${gridSizeNum}`;
 let squareColor = "black";
 let randomColorBtn = "off";
 
+// 1. Add the squares in the container
+
+let addSquares = function () {
+  const row = document.createElement("div");
+  row.classList.add("row");
+  container.append(row);
+  for (let i = 0; i < gridSizeNum; i++) {
+    const column = document.createElement("div");
+    column.classList.add("column");
+    row.append(column);
+  }
+  // 7. Change grid color on mouse enter
+
+  const squares = Array.from(document.querySelectorAll(".column"));
+  squares.forEach((square) =>
+    square.addEventListener("mouseenter", function (event) {
+      if (randomColorBtn == "off") {
+        event.target.style.backgroundColor = squareColor;
+      } else {
+        event.target.style.backgroundColor = rndColor();
+      }
+    })
+  );
+};
+// 2. Fill the Grid
+let gridFill = function (num) {
+  for (let i = 0; i < num; i++) {
+    addSquares();
+  }
+};
+
+// 3. Use provide grid size to fill de board
+
+gridFill(gridSizeNum);
+
 // 4. Prompt user the grid size of choice
 
 btnSize.addEventListener("click", function () {
@@ -50,41 +85,6 @@ clearBtn.addEventListener("click", function () {
   clearGrid();
 });
 
-// 1. Add the squares in the container
-
-let addSquares = function () {
-  const row = document.createElement("div");
-  row.classList.add("row");
-  container.append(row);
-  for (let i = 0; i < gridSizeNum; i++) {
-    const column = document.createElement("div");
-    column.classList.add("column");
-    row.append(column);
-  }
-  // 7. Change grid color on mouse enter
-
-  const squares = Array.from(document.querySelectorAll(".column"));
-  squares.forEach((square) =>
-    square.addEventListener("mouseenter", function (event) {
-      if (randomColorBtn == "off") {
-        event.target.style.backgroundColor = squareColor;
-      } else {
-        event.target.style.backgroundColor = rndColor();
-      }
-    })
-  );
-};
-// 2. Fill the Grid
-let gridFill = function (num) {
-  for (let i = 0; i < num; i++) {
-    addSquares();
-  }
-};
-
-// 3. Use provide grid size to fill de board
-
-gridFill(gridSizeNum);
-
 // 8. Create random color generator
 const rndColor = function () {
   let R = Math.floor(Math.random() * 256);
@@ -107,50 +107,3 @@ randomBtn.addEventListener("click", function () {
   }
   console.log(randomColorBtn);
 });
-
-// let userInput = "";
-// Add Grid = 16x16
-
-// for (let i = 1; i < 273; i++) {
-// Button prompt
-
-// btnSize.addEventListener("click", askGridSize);
-
-// function askGridSize() {
-//   userInput = parseInt(prompt("Select a grid size.", 16));
-//   if (typeof userInput === "undefined" || userInput > 100) {
-//     return;
-//   } else {
-//     grid(userInput);
-//   }
-// }
-// Grid
-
-// function grid(userInput) {
-//   if (userInput === 16) {
-//     for (let i = 1; i < 273; i++) {
-//       const div = document.createElement("div");
-//       div.classList.add("container-grid");
-//       container.append(div);
-//     }
-//   } else {
-//     for (let i = 1; i < userInput ** 2 + (userInput + 1); i++) {
-//       const div = document.createElement("div");
-//       div.classList.add("container-grid");
-//       container.append(div);
-//     }
-//   }
-// }
-
-// Select Divs inside container
-
-// const colorGrid = document.getElementsByClassName("column");
-// console.log(colorGrid.length);
-
-// // Event Listener for changing background color
-
-// for (let j = 0; j < colorGrid.length; j++) {
-//   colorGrid[j].addEventListener("mouseover", function () {
-//     colorGrid[j].style.backgroundColor = "black";
-//   });
-// }
